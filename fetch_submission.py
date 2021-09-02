cba0fd8e6765681e97ef60c0d851eecd693967a5
morikomorou <s.imayasu@hotmail.com>
2021-09-02 10:40:17 +0900

    add new file

--- fetch_submission.py
+++ fetch_submission.py
@@ -152,16 +152,16 @@ driver.quit()
 if add_cnt == 0:
     # 何も追加していなければGitにアクセスしない
     print("No added submissions, end process")
-else:
-    # GitHubにプッシュ
-    import git
-    import datetime
-
-    dt_now = datetime.datetime.now()
-    repo_url = "https://github.com/morikomorou/atcoder.git"
-    repo = git.Repo()
-    repo.git.add("submissions/*")
-    repo.git.commit("submissions/*", message="add submission: " + dt_now.strftime('%Y/%m/%d %H:%M:%S'))
-    repo.git.push("origin", "main")
-
-    print(f"Finished process, added {add_cnt} files")
+# else:
+#     # GitHubにプッシュ
+#     import git
+#     import datetime
+#
+#     dt_now = datetime.datetime.now()
+#     repo_url = "https://github.com/morikomorou/atcoder.git"
+#     repo = git.Repo()
+#     repo.git.add("submissions/*")
+#     repo.git.commit("submissions/*", message="add submission: " + dt_now.strftime('%Y/%m/%d %H:%M:%S'))
+#     repo.git.push("origin", "main")
+#
+#     print(f"Finished process, added {add_cnt} files")
